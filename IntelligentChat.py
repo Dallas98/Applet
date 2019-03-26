f7ed9c2e30b71d2fe4dfec5688c9c6db902386c0
Dallas98 <990259227@qq.com>
2019-03-26 21:23:06 +0800

    人脸检测、智能聊天

---
+++ IntelligentChat.py
@@ -0,0 +1,16 @@
+from wxpy import *
+
+bot = Bot(cache_path=True)
+
+my_friend = ensure_one(bot.search('Dallas'))
+
+turing = Tuling(api_key='7e239ea4ae684326a95e9cce4f99bd56')
+
+
+@bot.register(my_friend)
+def reply_my_friend(msg):
+    resp = turing.do_reply(msg)
+    print(msg, ' ', resp)
+
+
+embed()
